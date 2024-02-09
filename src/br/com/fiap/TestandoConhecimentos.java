613314ae3fa3518356175a9b76ec9f0cd63b1cc1
letyresina <leticiaresna@gmail.com>
2024-02-09 11:40:48 -0300

    questão 1 no código

--- src/br/com/fiap/TestandoConhecimentos.java
+++ src/br/com/fiap/TestandoConhecimentos.java
@@ -5,7 +5,11 @@ public class TestandoConhecimentos {
 		System.out.println("Testando os conhecimentos em Java");
 		
 		System.out.println("1. Explique Orientação à Objetos.");
-		System.out.println("");
+		System.out.println("Orientação à objetos seria uma forma de entender a complexidade do mundo"
+				+ " de maneira mais simples. Você abstrai, transformando em grupos, que possuem um "
+				+ "nome, atributos e metódos. Você entende completamente o problema antes de "
+				+ "codificar completamente, e isso também facilita o código, podendo reutilizar "
+				+ "conforme seu trabalho.");
 		
 		System.out.println("2. Explique o que é objeto.");
 		System.out.println("");
