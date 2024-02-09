cb3f12432a499e38bd784b1358d561e628fb505b
letyresina <leticiaresna@gmail.com>
2024-02-09 11:39:06 -0300

    subindo as perguntas

--- src/br/com/fiap/TestandoConhecimentos.java
+++ src/br/com/fiap/TestandoConhecimentos.java
@@ -3,5 +3,29 @@ package br.com.fiap;
 public class TestandoConhecimentos {
 	public static void main(String[] args) {
 		System.out.println("Testando os conhecimentos em Java");
+		
+		System.out.println("1. Explique Orientação à Objetos.");
+		System.out.println("");
+		
+		System.out.println("2. Explique o que é objeto.");
+		System.out.println("");
+		
+		System.out.println("3. Explique o que é classe.");
+		System.out.println("");
+		
+		System.out.println("4. Explique o que é abstração.");
+		System.out.println("");
+		
+		System.out.println("5. Explique o que é herança.");
+		System.out.println("");
+		
+		System.out.println("6. Explique o que é encapsulamento.");
+		System.out.println("");
+		
+		System.out.println("7. Explique o que é polimorfismo.");
+		System.out.println("");
+		
+		System.out.println("8. Quais são os principais tipos de polimorfismo? Explique cada um deles.");
+		System.out.println("");
 	}
 }
