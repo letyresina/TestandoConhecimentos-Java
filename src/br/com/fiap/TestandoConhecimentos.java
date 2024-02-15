21a15e5c084a5629b5e05dda955b8c76c38499dc
Leticia Resina <80417466+letyresina@users.noreply.github.com>
2024-02-15 08:39:25 -0300

    exercício 2 e 3 feitos

--- src/br/com/fiap/TestandoConhecimentos.java
+++ src/br/com/fiap/TestandoConhecimentos.java
@@ -13,10 +13,16 @@ public class TestandoConhecimentos {
 				+ "conforme seu trabalho.");
 		
 		System.out.println("2. Explique o que é objeto.");
-		System.out.println("");
+		System.out.println("Objeto é tudo aquilo que eu posso visualizar na vida real e posso" 
+				+ " representar através de classes. Como por exemplo, Letícia, Sofia e Joana, são" 
+				+ " exemplos de objetos que podem ser representados na classe Homem, que será utilizada"
+				+ " como exemplo na próxima questão.");
 		
 		System.out.println("3. Explique o que é classe.");
-		System.out.println("");
+		System.out.println("Toda classe são criadas através de objetos que possuem características em" 
+				+ " comum. A classe possui seus atributos com valores (que diferem entre si) e que" 
+				+ " tornam a classe ser o que é. Também possuem os metódos, que são os comportamentos" 
+				+ "que aquele objeto possui.");
 		
 		System.out.println("4. Explique o que é abstração.");
 		System.out.println("");
